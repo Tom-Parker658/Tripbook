742243103281fe5ea6071434607c843705581980
parkert805 <parkert805@gmailcom>
2021-08-10 22:37:25 +0200

    Job ------------------------------- Status AgencyConfigurationModule --------- 10% ConfigActivity.kt created and layout done! Hurray

---
+++ app/src/main/java/com/lado/travago/tripbook/ui/agency/creation/config/ConfigViewModel.kt
@@ -0,0 +1,7 @@
+package com.lado.travago.tripbook.ui.agency.creation.config
+
+import androidx.lifecycle.ViewModel
+
+class ConfigViewModel : ViewModel() {
+
+}
--- app/src/main/java/com/lado/travago/tripbook/utils/contracts/UserCreationContract.kt
+++ app/src/main/java/com/lado/travago/tripbook/utils/contracts/UserCreationContract.kt
@@ -15,37 +15,37 @@ import java.util.*
 /**
  * This is a contract to launch the scanner creation from the agency registration
  */
-@Suppress("KDocUnresolvedReference")
-@ExperimentalCoroutinesApi
-@InternalCoroutinesApi
-class UserCreationContract: ActivityResultContract<Pair<String?, String?>, User.UserBasicInfo>() {
-
-    /** Create an intent that can be used for [Activity.startActivityForResult]  */
-    override fun createIntent(context: Context, agencyNameToDBPathPair: Pair<String?, String?>): Intent {
-        val agencyName = agencyNameToDBPathPair.first
-        val agencyDBPath = agencyNameToDBPathPair.second
-        return Intent(context,  BookerCreationActivity::class.java)
-            .putExtra(AgencyCreationActivity.KEY_AGENCY_NAME, agencyName)
-            .putExtra(AgencyCreationActivity.KEY_OTA_PATH, agencyDBPath)
-    }
-
-    /** Convert result obtained from [Activity.onActivityResult] to Output  */
-    override fun parseResult(resultCode: Int, infoIntent: Intent?) = when(resultCode){
-        Activity.RESULT_OK -> {
-            User.UserBasicInfo(
-                name = infoIntent?.getStringExtra(AgencyCreationActivity.KEY_SCANNER_NAME) ?: "name",
-                phoneNumber = infoIntent?.getStringExtra(AgencyCreationActivity.KEY_SCANNER_PHONE) ?: "655223344",
-                photoUrl = infoIntent?.getStringExtra(AgencyCreationActivity.KEY_SCANNER_URL) ?: "ss",
-                birthdayInMillis = infoIntent?.getLongExtra(AgencyCreationActivity.KEY_SCANNER_BIRTHDAY, Date().time) ?: 0L,
-                occupation = OCCUPATION.SCANNER
-            )
-        }
-        else -> User.UserBasicInfo(
-            "",
-            Date().time,
-            "",
-            "",
-            OCCUPATION.SCANNER
-        )
-    }
-}
+//@Suppress("KDocUnresolvedReference")
+//@ExperimentalCoroutinesApi
+//@InternalCoroutinesApi
+//class UserCreationContract: ActivityResultContract<Pair<String?, String?>, User.UserBasicInfo>() {
+//
+//    /** Create an intent that can be used for [Activity.startActivityForResult]  */
+//    override fun createIntent(context: Context, agencyNameToDBPathPair: Pair<String?, String?>): Intent {
+//        val agencyName = agencyNameToDBPathPair.first
+//        val agencyDBPath = agencyNameToDBPathPair.second
+//        return Intent(context,  BookerCreationActivity::class.java)
+//            .putExtra(AgencyCreationActivity.KEY_AGENCY_NAME, agencyName)
+//            .putExtra(AgencyCreationActivity.KEY_OTA_PATH, agencyDBPath)
+//    }
+//
+//    /** Convert result obtained from [Activity.onActivityResult] to Output  */
+//    override fun parseResult(resultCode: Int, infoIntent: Intent?) = when(resultCode){
+//        Activity.RESULT_OK -> {
+//            User.UserBasicInfo(
+//                name = infoIntent?.getStringExtra(AgencyCreationActivity.KEY_SCANNER_NAME) ?: "name",
+//                phoneNumber = infoIntent?.getStringExtra(AgencyCreationActivity.KEY_SCANNER_PHONE) ?: "655223344",
+//                photoUrl = infoIntent?.getStringExtra(AgencyCreationActivity.KEY_SCANNER_URL) ?: "ss",
+//                birthdayInMillis = infoIntent?.getLongExtra(AgencyCreationActivity.KEY_SCANNER_BIRTHDAY, Date().time) ?: 0L,
+//                occupation = OCCUPATION.SCANNER
+//            )
+//        }
+//        else -> User.UserBasicInfo(
+//            "",
+//            Date().time,
+//            "",
+//            "",
+//            OCCUPATION.SCANNER
+//        )
+//    }
+//}
